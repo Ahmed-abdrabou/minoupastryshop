a46ca45bd0ce75158157aacf1962b0e227f615fc
ahmed-abdrabou <kush.boy498@gmail.com>
2023-03-24 06:13:04 +0200

    deployed website 4

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,31 +1,36 @@
 import React from "react";
-
-// import { images } from "../../constants";
+import { useNavigate } from "react-router-dom";
 import "./Header.css";
 
 import BackGroundSlider from "../BackGroundSlider/BackGroundSlider";
 import { BackGround_images } from "../../constants/BackGround_images";
 
-const Header = () => (
-  <div className="app__content app__header" id="Header">
-    <div className="app__header-text">
-      <h2>Cake Your Day;</h2>
-      <p>
-        For our desserts we always use only the natural ingredients, Our
-        production is 100% handmade
-      </p>
-      <p>Made For You With Love...&hearts;</p>
+const Header = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="app__content app__header" id="Header">
+      <div className="app__header-text">
+        <h2>Cake Your Day;</h2>
+        <p>
+          For our desserts we always use only the natural ingredients, Our
+          production is 100% handmade
+        </p>
+        <p>Made For You With Love...&hearts;</p>
 
-      <span className="custom__button header-foot_button">
-        <a href="#OrderNow">Order Now..</a>
-      </span>
-    </div>
+        <span
+          className="custom__button header-foot_button"
+          onClick={() => navigate("/OrderNow")}
+        >
+          Order Now..
+        </span>
+      </div>
 
-    <BackGroundSlider slides={BackGround_images} />
-    <div className="header-foot">
-      <span className="header-foot-bg"></span>
+      <BackGroundSlider slides={BackGround_images} />
+      <div className="header-foot">
+        <span className="header-foot-bg"></span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Header;
--- src/container/WeddingCakes/WeddingCakes.jsx
+++ src/container/WeddingCakes/WeddingCakes.jsx
@@ -1,34 +1,38 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import "./WeddingCakes.css";
 
 import { WeddingCakes_img } from "../../constants/WeddingCakes_img";
 
-const WeddingCakes = () => (
-  <div className="section__padding section_container Small_flex_column_reverse WeddingCakes">
-    <div className="section_images_container">
-      {WeddingCakes_img.map((card, index) => {
-        return (
-          <div className="section_img" key={index}>
-            <img src={card.image} alt="section_img" />
-          </div>
-        );
-      })}
+const WeddingCakes = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="section__padding section_container Small_flex_column_reverse WeddingCakes">
+      <div className="section_images_container">
+        {WeddingCakes_img.map((card, index) => {
+          return (
+            <div className="section_img" key={index}>
+              <img src={card.image} alt="section_img" />
+            </div>
+          );
+        })}
+      </div>
+      <div className="section_Text_container">
+        <h2>Wedding Cakes</h2>
+        <h3>Make your occasion extra special</h3>
+        <p>
+          We offer specially designed cakes for those of you looking for more
+          than an ordinary cake. Whether you are throwing a surprise party,
+          celebrating a birthday, engagement or getting married, we are sure to
+          create a beautiful and unique cake for your special occasion.
+        </p>
+        <span className="custom__button " onClick={() => navigate("/OrderNow")}>
+          Order Now..
+        </span>
+      </div>
     </div>
-    <div className="section_Text_container">
-      <h2>Wedding Cakes</h2>
-      <h3>Make your occasion extra special</h3>
-      <p>
-        We offer specially designed cakes for those of you looking for more than
-        an ordinary cake. Whether you are throwing a surprise party, celebrating
-        a birthday, engagement or getting married, we are sure to create a
-        beautiful and unique cake for your special occasion.
-      </p>
-      <span className="custom__button section_container_button">
-        Order Now..
-      </span>
-    </div>
-  </div>
-);
+  );
+};
 
 export default WeddingCakes;
